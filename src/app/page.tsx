a2594b91fb4adbb30f8a98eb359badbc1ff229db
kurimaruru <kurikuribaseball530@gmail.com>
2024-07-06 13:18:48 +0900

    feat: Home画面のレスポンシブ対応

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,77 +24,77 @@ export default function Home() {
           <div
             className={`main fixed top-0 left-0 w-full h-screen animate-fadeIn`}
           >
-            <div className="flex flex-wrap fixed top-20 w-full h-[calc(100%-80px)] overflow-y-scroll hidden-scrollbar">
-              <div className="w-1/3">
+            <div className="grid grid-cols-3 gap-1 lg:grid-cols-6 fixed top-20 w-full h-[calc(100%-80px)] overflow-y-scroll hidden-scrollbar">
+              <div className="col-span-1 lg:col-span-1 order-1">
                 <Image
                   src="/Home/home_1.jpg"
                   alt={"home_1"}
                   width={1080}
                   height={1616}
-                  className="w-full h-[200px]"
+                  className="w-full h-full object-cover"
                 />
               </div>
-              <div className="w-2/3 pl-1">
+              <div className="col-span-2 lg:col-span-2 order-2">
                 <Image
                   src="/Home/home_2.jpg"
                   alt={"home_2"}
                   width={1080}
                   height={1616}
-                  className="w-full h-[200px]"
+                  className="w-full h-full object-cover"
                 />
               </div>
-              <div className="w-2/3 pr-1 pt-1">
+              <div className="col-span-2 lg:col-span-2 order-3 lg:order-4">
                 <Image
                   src="/Home/home_3.jpg"
                   alt={"home_3"}
                   width={1080}
                   height={1616}
-                  className="w-full h-[200px]"
+                  className="w-full h-full object-cover"
                 />
               </div>
-              <div className="w-1/3  pt-1">
+              <div className="col-span-1 lg:col-span-1 order-4 lg:order-3">
                 <Image
                   src="/Home/home_4.jpg"
                   alt={"home_4"}
                   width={1080}
                   height={1616}
-                  className="w-full h-[200px]"
+                  className="w-full h-full object-cover"
                 />
               </div>
-              <div className="w-1/3 pt-1">
+              <div className="col-span-1 lg:col-span-1 order-5">
                 <Image
                   src="/Home/home_5.jpg"
                   alt={"home_5"}
                   width={1080}
                   height={1616}
-                  className="w-full h-[200px]"
+                  className="w-full h-full object-cover"
                 />
               </div>
-              <div className="w-2/3 pl-1 pt-1">
+              <div className="col-span-2 lg:col-span-2 order-6">
                 <Image
                   src="/Home/home_6.jpg"
                   alt={"home_6"}
                   width={1080}
                   height={1616}
-                  className="w-full h-[200px]"
+                  className="w-full h-full object-cover"
                 />
               </div>
-              <div className="w-2/3 pr-1 pt-1">
+              <div className="col-span-2 lg:col-span-2 order-7 lg:order-8">
                 <Image
                   src="/Home/home_7.jpg"
                   alt={"home_7"}
                   width={1080}
                   height={1616}
-                  className="w-full h-[200px]"
+                  className="w-full h-full object-cover"
                 />
               </div>
-              <div className="w-1/3  pt-1">
+              <div className="col-span-1 lg:col-span-1 order-8 lg:order-7">
                 <Image
                   src="/Home/home_8.jpg"
                   alt={"home_8"}
                   width={1080}
                   height={1616}
-                  className="w-full h-[200px]"
+                  className="w-full h-full object-cover"
                 />
               </div>
             </div>

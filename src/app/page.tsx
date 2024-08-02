d5b64fd4e1cee6d6b75f853eaf933d1e7af7b0a4
kurimaruru <kurikuribaseball530@gmail.com>
2024-08-03 07:15:35 +0900

    update: HOME画面の更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,73 +27,73 @@ export default function Home() {
             <div className="grid grid-cols-3 gap-1 lg:grid-cols-6 fixed top-20 w-full h-[calc(100%-80px)] overflow-y-scroll hidden-scrollbar">
               <div className="col-span-1 lg:col-span-1 order-1">
                 <Image
-                  src="/Home/home_1.jpg"
+                  src="/Home/home_1.JPG"
                   alt={"home_1"}
-                  width={1080}
-                  height={1616}
+                  width={600}
+                  height={900}
                   className="w-full h-full object-cover"
                 />
               </div>
               <div className="col-span-2 lg:col-span-2 order-2">
                 <Image
-                  src="/Home/home_2.jpg"
+                  src="/Home/home_2.JPG"
                   alt={"home_2"}
-                  width={1080}
-                  height={1616}
+                  width={900}
+                  height={600}
                   className="w-full h-full object-cover"
                 />
               </div>
               <div className="col-span-2 lg:col-span-2 order-3 lg:order-4">
                 <Image
-                  src="/Home/home_3.jpg"
+                  src="/Home/home_3.JPG"
                   alt={"home_3"}
-                  width={1080}
-                  height={1616}
+                  width={600}
+                  height={900}
                   className="w-full h-full object-cover"
                 />
               </div>
               <div className="col-span-1 lg:col-span-1 order-4 lg:order-3">
                 <Image
-                  src="/Home/home_4.jpg"
+                  src="/Home/home_4.JPG"
                   alt={"home_4"}
-                  width={1080}
-                  height={1616}
+                  width={900}
+                  height={600}
                   className="w-full h-full object-cover"
                 />
               </div>
               <div className="col-span-1 lg:col-span-1 order-5 lg:order-6">
                 <Image
-                  src="/Home/home_5.jpg"
+                  src="/Home/home_5.JPG"
                   alt={"home_5"}
-                  width={1080}
-                  height={1616}
+                  width={600}
+                  height={900}
                   className="w-full h-full object-cover"
                 />
               </div>
               <div className="col-span-2 lg:col-span-2 order-6 lg:order-5">
                 <Image
-                  src="/Home/home_6.jpg"
+                  src="/Home/home_6.JPG"
                   alt={"home_6"}
-                  width={1080}
-                  height={1616}
+                  width={900}
+                  height={600}
                   className="w-full h-full object-cover"
                 />
               </div>
               <div className="col-span-2 lg:col-span-2 order-7">
                 <Image
-                  src="/Home/home_7.jpg"
+                  src="/Home/home_7.JPG"
                   alt={"home_7"}
-                  width={1080}
-                  height={1616}
+                  width={600}
+                  height={900}
                   className="w-full h-full object-cover"
                 />
               </div>
               <div className="col-span-1 lg:col-span-1 order-8">
                 <Image
-                  src="/Home/home_8.jpg"
+                  src="/Home/home_8.JPG"
                   alt={"home_8"}
-                  width={1080}
-                  height={1616}
+                  width={900}
+                  height={600}
                   className="w-full h-full object-cover"
                 />
               </div>
